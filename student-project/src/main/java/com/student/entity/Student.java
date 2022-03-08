56988ef1530addb4de75d66b32083f1488f900ad
Your Name <you@example.com>
2022-03-08 20:51:22 +0530

    initail spring boot application start

---
+++ student-project/src/main/java/com/student/entity/Student.java
@@ -0,0 +1,60 @@
+package com.student.entity;
+
+public class Student {
+
+	private Integer studentId;
+	private String studentName;
+	private String mobileNumber;
+	private String email;
+	private String loginId;
+	private String password;
+
+	public Integer getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(Integer studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getLoginId() {
+		return loginId;
+	}
+
+	public void setLoginId(String loginId) {
+		this.loginId = loginId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
